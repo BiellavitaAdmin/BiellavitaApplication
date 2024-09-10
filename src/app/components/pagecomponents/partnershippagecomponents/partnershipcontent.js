c2c2a2fef4e51d604caa66e02e65383b36b9417c
aitizaz <atz.softprgmr@gmail.com>
2024-09-10 13:28:56 +0500

    added changes

--- src/app/components/pagecomponents/partnershippagecomponents/partnershipcontent.js
+++ src/app/components/pagecomponents/partnershippagecomponents/partnershipcontent.js
@@ -15,7 +15,12 @@ export default function PartnershipContent() {
       <div className="partnership-content">
         <article className="section-content">
           <ParaOne />
-          <ParaTwo />
+          <p className="partnership-page-content-text">
+            By embracing the core values that define BiellaVita—such as
+            integrity, innovative, education and truthful—these collaborations
+            will not only enhance the brand's mission but also create synergies
+            that benefit all parties involved.
+          </p>
           <DualPicContiner />
           <ParaThree />
           <ParaFour />
---
+++ src/app/components/pagecomponents/partnershippagecomponents/partnershipsubcomponents/paranoi.js
@@ -0,0 +1,10 @@
+export default function ParaNoi() {
+  <>
+    <p className="partnership-page-content-text">
+      By embracing the core values that define BiellaVita—such as integrity,
+      innovative, education and truthful—these collaborations will not only
+      enhance the brand's mission but also create synergies that benefit all
+      parties involved.
+    </p>
+  </>;
+}
