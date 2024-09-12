1e65bf447148324786ade52cf368471e789a9e3a
aitizaz <atz.softprgmr@gmail.com>
2024-09-12 12:22:52 +0500

    added changes

--- src/app/admin/components/subcomponents/headerleftsection.js
+++ src/app/admin/components/subcomponents/headerleftsection.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function HeaderLeftSection() {
   return (
     <>
       <div className="header-left-section">
-        <Image
-          src="/Monogram.webp"
-          alt="My Awesome Image"
-          width={85}
-          height={85}
-        />
+        <Link href="/">
+          <Image
+            src="/Monogram.webp"
+            alt="My Awesome Image"
+            width={85}
+            height={85}
+          />
+        </Link>
       </div>
     </>
   );
