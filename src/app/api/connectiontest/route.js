44d7385cea126f05c43079d9e8894a945cf5dc2c
aitizaz <atz.softprgmr@gmail.com>
2024-09-23 11:28:56 +0500

    added mongodb database

---
+++ src/app/api/connectiontest/route.js
@@ -0,0 +1,30 @@
+import clientPromise from "../../../lib/mongodb";
+
+export async function GET(req) {
+  try {
+    const client = await clientPromise; // This line establishes the connection
+    return new Response(
+      JSON.stringify({ message: "MongoDB connected successfully!" }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    return new Response(
+      JSON.stringify({
+        message: "MongoDB connection error",
+        error: error.message,
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
---
+++ src/lib/mongodb.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_DB_URI; // Replace with your connection string
+
+let client;
+let clientPromise;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client
+      .connect()
+      .then(() => {
+        console.log("MongoDB connected successfully!");
+        return client;
+      })
+      .catch((err) => {
+        console.error("MongoDB connection error:", err);
+      });
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client
+    .connect()
+    .then(() => {
+      console.log("MongoDB connected successfully!");
+      return client;
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+    });
+}
+
+export default clientPromise;
