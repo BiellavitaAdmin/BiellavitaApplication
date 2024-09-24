91673a17771b007b361636d9f598794fd365f7d2
aitizaz <atz.softprgmr@gmail.com>
2024-09-24 18:28:21 +0500

    added changes

--- src/app/admin/projects/addprojects/page.js
+++ src/app/admin/projects/addprojects/page.js
@@ -1,36 +1,219 @@
-import "./addprojects.css";
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Alert } from "antd";
+import "../../members/addmembers/addmembers.css";
+
 export default function AddProjects() {
+  const [formData, setFormData] = useState({
+    projecttitle: "",
+    imagelink: "",
+    shortdescription: "",
+    details: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false); // Track form submission
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  // Input change handler
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Alphabetic validation for projecttitle
+    if (name === "projecttitle") {
+      if (/[^a-zA-Z\s]/.test(value)) {
+        setErrors({
+          ...errors,
+          [name]: "Only alphabetic characters are allowed",
+        });
+      } else {
+        setErrors({ ...errors, [name]: "" });
+      }
+    }
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Validate form fields
+  const validateForm = () => {
+    const newErrors = {};
+    if (!formData.projecttitle)
+      newErrors.projecttitle = "Project title is required";
+    if (!formData.imagelink) newErrors.imagelink = "Project title is required";
+    if (!formData.shortdescription)
+      newErrors.shortdescription = "Short description is required";
+    if (!formData.details) newErrors.details = "Details are required";
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  // Submit handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    try {
+      const res = await fetch("/api/projects", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (res.ok) {
+        setSuccessMessage("Project added successfully!");
+        setFormData({
+          projecttitle: "",
+          imagelink: "",
+          shortdescription: "",
+          details: "",
+        });
+        setErrors({});
+        setIsSubmitted(true);
+        setErrorMessage(null);
+
+        // Remove success message after 3 seconds
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 3000);
+      } else {
+        setErrorMessage("Failed to add project.");
+        setSuccessMessage(null);
+
+        // Remove error message after 3 seconds
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 3000);
+      }
+    } catch (err) {
+      console.error(err);
+      setErrorMessage("An error occurred.");
+      setSuccessMessage(null);
+
+      // Remove error message after 3 seconds
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+    }
+  };
+
   return (
-    <>
-      <div className="form-container">
-        <div className="form-section">
-          <form className="dash-form">
-            <div className="dash-inputfield-group-column">
-              <label className="dash-form-label">Firstname</label>
-              <input className="dash-form-input"></input>
-            </div>
-            <div className="dash-inputfield-group-column">
-              <label className="dash-form-label">Lastname</label>
-              <input className="dash-form-input"></input>
-            </div>
-            <div className="dash-inputfield-group-column">
-              <label className="dash-form-label">Address</label>
-              <input className="dash-form-input"></input>
-            </div>
-            <div className="dash-inputfield-group-column">
-              <label className="dash-form-label">Cell Phone</label>
-              <input className="dash-form-input"></input>
-            </div>
-            <div className="dash-inputfield-group-column">
-              <label className="dash-form-label">Email</label>
-              <input className="dash-form-input"></input>
-            </div>
-            <div className="dash-form-conatiner">
-              <button className="dash-form-button">Add Projects</button>
-            </div>
-          </form>
+    <div className="form-container">
+      {/* Display Antd Alert for errors */}
+      {errorMessage && (
+        <Alert
+          message={errorMessage}
+          type="error"
+          showIcon
+          style={{
+            width: "20%",
+            marginBottom: "16px",
+            textAlign: "right",
+          }}
+        />
+      )}
+
+      {/* Display Antd Alert for success */}
+      {successMessage && (
+        <div className="alert-container">
+          <Alert
+            message={successMessage}
+            type="success"
+            showIcon
+            style={{
+              width: "20%",
+              marginBottom: "16px",
+            }}
+          />
         </div>
+      )}
+
+      <div className="form-section">
+        <form className="dash-form" onSubmit={handleSubmit}>
+          {/* Project Title */}
+          <div className="dash-inputfield-group-column">
+            <label className="dash-form-label">Project Title</label>
+            <input
+              className={`dash-form-input ${
+                errors.projecttitle ? "input-error" : ""
+              }`}
+              name="projecttitle"
+              placeholder="Enter project title"
+              value={formData.projecttitle}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.projecttitle && (
+            <p className="error-message">{errors.projecttitle}</p>
+          )}
+
+          <div className="dash-inputfield-group-column">
+            <label className="dash-form-label">Image Link</label>
+            <input
+              className={`dash-form-input ${
+                errors.imagelink ? "input-error" : ""
+              }`}
+              name="imagelink"
+              placeholder="Enter image title"
+              value={formData.imagelink}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.imagelink && (
+            <p className="error-message">{errors.imagelink}</p>
+          )}
+
+          {/* Short Description */}
+          <div className="dash-inputfield-group-column">
+            <label className="dash-form-label">Short Description</label>
+            <textarea
+              className={`dash-form-input-message ${
+                errors.shortdescription ? "input-error" : ""
+              }`}
+              name="shortdescription"
+              placeholder="Enter a short description of the project"
+              rows={15}
+              value={formData.shortdescription}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.shortdescription && (
+            <p className="error-message">{errors.shortdescription}</p>
+          )}
+
+          {/* Details */}
+          <div className="dash-inputfield-group-column">
+            <label className="dash-form-label">Details</label>
+            <textarea
+              className={`dash-form-input-message-larger ${
+                errors.details ? "input-error" : ""
+              }`}
+              name="details"
+              placeholder="Enter project details"
+              rows={15}
+              value={formData.details}
+              onChange={handleChange}
+            />
+          </div>
+          {errors.details && <p className="error-message">{errors.details}</p>}
+
+          <div className="dash-form-container">
+            <button className="dash-form-button" type="submit">
+              Add Project
+            </button>
+            {isSubmitted && (
+              <Link href="/admin/projects">
+                <button className="dash-form-button goto">
+                  Back to Projects
+                </button>
+              </Link>
+            )}
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
---
+++ src/app/api/projects/route.js
@@ -0,0 +1,109 @@
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export async function GET() {
+  const client = await clientPromise;
+  const db = client.db();
+
+  try {
+    const projects = await db.collection("projects").find({}).toArray();
+    return new Response(JSON.stringify(projects), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    return new Response(
+      JSON.stringify({
+        message: "Error fetching projects",
+        error: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req) {
+  const projectData = await req.json();
+  const client = await clientPromise;
+  const db = client.db();
+
+  try {
+    const result = await db.collection("projects").insertOne(projectData);
+    return new Response(
+      JSON.stringify({ message: "Project added successfully!" }),
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error adding project:", error);
+    return new Response(
+      JSON.stringify({ message: "Error adding project", error: error.message }),
+      { status: 500 }
+    );
+  }
+}
+
+// Edit Member API
+export async function PUT(req) {
+  const { id, ...updateData } = await req.json();
+  const client = await clientPromise;
+  const db = client.db();
+
+  try {
+    const result = await db
+      .collection("projects")
+      .updateOne({ _id: new ObjectId(id) }, { $set: updateData });
+
+    if (result.modifiedCount === 0) {
+      return new Response(
+        JSON.stringify({ message: "No project found with that ID" }),
+        { status: 404 }
+      );
+    }
+
+    return new Response(
+      JSON.stringify({ message: "project updated successfully!" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating project:", error);
+    return new Response(
+      JSON.stringify({
+        message: "Error updating project",
+        error: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
+
+// Delete Member API
+export async function DELETE(req) {
+  const { id } = await req.json();
+  const client = await clientPromise;
+  const db = client.db();
+
+  try {
+    const result = await db
+      .collection("projects")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return new Response(
+        JSON.stringify({ message: "No project found with that ID" }),
+        { status: 404 }
+      );
+    }
+
+    return new Response(
+      JSON.stringify({ message: "project deleted successfully!" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    return new Response(
+      JSON.stringify({
+        message: "Error deleting project",
+        error: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
