9c132bdd8ef31c6b0df30b9bfc07d4425aa35fe1
aitizaz <atz.softprgmr@gmail.com>
2024-09-24 11:03:46 +0500

    added changes

--- src/app/components/pagecomponents/pagefootercomponents/subcomponenets/leftsectionaddresssection.js
+++ src/app/components/pagecomponents/pagefootercomponents/subcomponenets/leftsectionaddresssection.js
@@ -1,9 +1,9 @@
 export default function LeftSectionAddressNav() {
   return (
-    <div className="address-section">
-      <p className="address-text">53 Davies Street</p>
-      <p className="address-text">London</p>
-      <p className="address-text">W1K 5JH</p>
+    <div className="address-section-full-page-footer">
+      <p className="address-text-full-page-footer">53 Davies Street</p>
+      <p className="address-text-full-page-footer">London</p>
+      <p className="address-text-full-page-footer">W1K 5JH</p>
     </div>
   );
 }
