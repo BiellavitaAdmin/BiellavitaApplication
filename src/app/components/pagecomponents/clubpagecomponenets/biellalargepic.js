e4110c28b7647c8440d83f797516d4fbd5b5079b
aitizaz <atz.softprgmr@gmail.com>
2024-09-24 10:42:45 +0500

    added changes

--- src/app/components/pagecomponents/clubpagecomponenets/biellalargepic.js
+++ src/app/components/pagecomponents/clubpagecomponenets/biellalargepic.js
@@ -1,6 +1,16 @@
+import Image from "next/image";
 export default function BiellaLargePic() {
   return (
     <>
+      <div className="large-pic-continer-club-page">
+        <Image
+          src="/Biella-lake.png"
+          alt="My Awesome Image"
+          width={100}
+          height={835}
+          className="club-page-large-image-settings"
+        />
+      </div>
       <h2 className="clubpage-heading">The source of the idea</h2>
       <p className="page-content-text">
         … is the inspirational city of Biella.
--- src/app/components/pagecomponents/clubpagecomponenets/sourcesectionparafour.js
+++ src/app/components/pagecomponents/clubpagecomponenets/sourcesectionparafour.js
@@ -1,6 +1,24 @@
+import Image from "next/image";
 export default function SourceSectionParaFour() {
   return (
     <>
+      <div className="dual-pic-continer-club-page">
+        <Image
+          src="/clubpage-left.png"
+          alt="My Awesome Image"
+          width={100}
+          height={835}
+          className="club-page-dual-image-settings-one"
+        />
+        <Image
+          src="/clubpage-right.png"
+          alt="My Awesome Image"
+          width={100}
+          height={835}
+          className="club-page-dual-image-settings-two"
+        />
+      </div>
+
       <p className="page-content-text">
         Named the “Capital of Wool” its picturesque valleys are adorned with
         renowned woolen mills.
