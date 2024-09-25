227c0d0464ce8dff3d7a31b7b6c677b760e77272
aitizaz <atz.softprgmr@gmail.com>
2024-09-25 19:24:57 +0500

    added changes

--- src/app/components/pagecomponents/privateeventspage/subcomponents/largepiccontainer.js
+++ src/app/components/pagecomponents/privateeventspage/subcomponents/largepiccontainer.js
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function LargePicContainer({ imageUrl }) {
   return (
     <div className="privateevents-large-pic-container">
-      <Image src={imageUrl} alt="Event Image" width={1550} height={785} />
+      <Image
+        src={imageUrl}
+        alt="Event Image"
+        width={1550}
+        height={785}
+        className="upcoming-large-image"
+      />
     </div>
   );
 }
--- src/app/components/pagecomponents/privateeventspage/subcomponents/previouseventscards.js
+++ src/app/components/pagecomponents/privateeventspage/subcomponents/previouseventscards.js
@@ -1,19 +1,53 @@
+import { Modal } from "antd";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function PreviousEventsCards({ event }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <div className="previous-events-card">
-      <div className="previous-event-large-pic">
-        <Image
-          src={event.imagelink}
-          alt={event.eventtitle}
-          width={450}
-          height={320}
-        />
+    <>
+      <div className="previous-events-card">
+        <div className="previous-event-large-pic">
+          <Image
+            src={event.imagelink}
+            alt={event.eventtitle}
+            width={450}
+            height={320}
+            className="previous-event-large-image-settings"
+          />
+        </div>
+        <h2 className="previous-card-heading space-left">{event.eventtitle}</h2>
+        <div className="previous-event-description space-left">
+          {event.shortdescription}
+        </div>
+        <button
+          className="previous-event-details-button space-left"
+          onClick={showModal}
+        >
+          Discover More
+        </button>
       </div>
-      <h2 className="previous-card-heading">{event.eventtitle}</h2>
-      <div className="previous-event-description">{event.shortdescription}</div>
-      <button className="previous-event-details-button">Discover More</button>
-    </div>
+
+      {/* Modal for displaying event details */}
+      <Modal
+        title={event.eventtitle}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <div>
+          <p>{event.details}</p>
+        </div>
+      </Modal>
+    </>
   );
 }
