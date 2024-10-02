1280af185e199a19bc93e91771247cbba1510f28
aitizaz <atz.softprgmr@gmail.com>
2024-10-02 10:34:19 +0500

    added changes

--- src/app/components/pagecomponents/clubpagecomponenets/sourcesection.js
+++ src/app/components/pagecomponents/clubpagecomponenets/sourcesection.js
@@ -1,24 +1,48 @@
-import SourceSectionParaOne from "./sourcesectionparaone";
-import SourceSectionParaTwo from "./sourcesectionparatwo";
-import SourceSectionParaThree from "./sourcesectionparathree";
-import SourceSectionParaFour from "./sourcesectionparafour";
-import SourceSectionParaFive from "./sourcesectionparafive";
-import SourceSectionParaSix from "./sourcesectionparasix";
-import SourceSectionParaSeven from "./sourcesectionparaseven";
-import BiellaLargePic from "./biellalargepic";
+import dynamic from "next/dynamic";
+
+// Dynamically import each component
+const SourceSectionParaOne = dynamic(() => import("./sourcesectionparaone"), {
+  ssr: false,
+});
+const SourceSectionParaTwo = dynamic(() => import("./sourcesectionparatwo"), {
+  ssr: false,
+});
+const SourceSectionParaThree = dynamic(
+  () => import("./sourcesectionparathree"),
+  {
+    ssr: false,
+  }
+);
+const SourceSectionParaFour = dynamic(() => import("./sourcesectionparafour"), {
+  ssr: false,
+});
+const SourceSectionParaFive = dynamic(() => import("./sourcesectionparafive"), {
+  ssr: false,
+});
+const SourceSectionParaSix = dynamic(() => import("./sourcesectionparasix"), {
+  ssr: false,
+});
+const SourceSectionParaSeven = dynamic(
+  () => import("./sourcesectionparaseven"),
+  {
+    ssr: false,
+  }
+);
+const BiellaLargePic = dynamic(() => import("./biellalargepic"), {
+  ssr: false,
+});
+
 export default function SourceSection() {
   return (
-    <>
-      <article className="section-content">
-        <SourceSectionParaOne />
-        <SourceSectionParaTwo />
-        <BiellaLargePic />
-        <SourceSectionParaThree />
-        <SourceSectionParaFour />
-        <SourceSectionParaFive />
-        <SourceSectionParaSix />
-        <SourceSectionParaSeven />
-      </article>
-    </>
+    <article className="section-content">
+      <SourceSectionParaOne />
+      <SourceSectionParaTwo />
+      <BiellaLargePic />
+      <SourceSectionParaThree />
+      <SourceSectionParaFour />
+      <SourceSectionParaFive />
+      <SourceSectionParaSix />
+      <SourceSectionParaSeven />
+    </article>
   );
 }
