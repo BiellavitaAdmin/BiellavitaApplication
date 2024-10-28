1b6e9c3194dbf7507c6a65ccc2c23a6c4ea9c955
aitizaz <atz.softprgmr@gmail.com>
2024-10-28 12:38:11 +0500

    added changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,40 +4,40 @@ import "./page.css";
 import Head from "next/head";
 
 // Add metadata directly to the page.js
-// export const metadata = {
-//   title: "Home | BiellaVita",
-//   description:
-//     "BiellaVita is born from the desire to connect like-minded individuals who embrace a lifestyle centered around excellence, reverence for nature, well-being, and a deep appreciation for the artistic, aesthetic, culinary and spiritual dimensions of life.",
-//   canonical: "https://www.biellavita.com",
-//   icons: {
-//     icon: "https://i.imgur.com/5sQQ2bY.png",
-//     apple: "https://i.imgur.com/5sQQ2bY.png",
-//   },
-//   openGraph: {
-//     type: "website",
-//     url: "https://www.biellavita.com",
-//     title: "Connect Through BiellaVita | A Private Social Club",
-//     description:
-//       "Join BiellaVita, a club for individuals who value excellence, nature, well-being, and the aesthetic and spiritual aspects of life.",
-//     images: [
-//       {
-//         url: "https://i.imgur.com/5sQQ2bY.png",
-//         width: 1200,
-//         height: 630,
-//         alt: "Biella Vita - A Private Social Club",
-//       },
-//     ],
-//     site_name: "BiellaVita",
-//     locale: "en_US",
-//     "article:section": "Lifestyle",
-//     "article:tag": ["Social Club", "Well-being", "Lifestyle"],
-//   },
-// };
+export const metadata = {
+  title: "Home | BiellaVita",
+  description:
+    "BiellaVita is born from the desire to connect like-minded individuals who embrace a lifestyle centered around excellence, reverence for nature, well-being, and a deep appreciation for the artistic, aesthetic, culinary and spiritual dimensions of life.",
+  canonical: "https://www.biellavita.com",
+  icons: {
+    icon: "https://i.imgur.com/5sQQ2bY.png",
+    apple: "https://i.imgur.com/5sQQ2bY.png",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://www.biellavita.com",
+    title: "Connect Through BiellaVita | A Private Social Club",
+    description:
+      "Join BiellaVita, a club for individuals who value excellence, nature, well-being, and the aesthetic and spiritual aspects of life.",
+    images: [
+      {
+        url: "https://i.imgur.com/5sQQ2bY.png",
+        width: 1200,
+        height: 630,
+        alt: "Biella Vita - A Private Social Club",
+      },
+    ],
+    site_name: "BiellaVita",
+    locale: "en_US",
+    "article:section": "Lifestyle",
+    "article:tag": ["Social Club", "Well-being", "Lifestyle"],
+  },
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Home | BiellaVita</title>
         <meta
           name="description"
@@ -52,7 +52,7 @@ export default function Home() {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:url" content="https://www.biellavita.com/club" />
-      </Head>
+      </Head> */}
       <main className="homepage">
         {/* <video autoPlay muted loop className="background-video">
         <source src="/Slowed background.mp4" type="video/mp4" />
