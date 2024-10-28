d4df79ac7c1e705dc6af98525b3ba1c6aa8a7a64
aitizaz <atz.softprgmr@gmail.com>
2024-10-28 12:12:00 +0500

    added changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import MainHeading from "./components/pagecomponents/homepage/mainheading";
 import Footer from "./components/sharedcomponents/footer";
 import "./page.css";
+import Head from "next/head";
 
 // Add metadata directly to the page.js
 // export const metadata = {
@@ -35,14 +36,32 @@ import "./page.css";
 
 export default function Home() {
   return (
-    <main className="homepage">
-      {/* <video autoPlay muted loop className="background-video">
+    <>
+      <Head>
+        <title>Home | BiellaVita</title>
+        <meta
+          name="description"
+          content="Join BiellaVita, a club for individuals who value excellence, nature, and well-being."
+        />
+        <meta property="og:title" content="The Club | BiellaVita" />
+        <meta
+          property="og:description"
+          content="Experience the BiellaVita lifestyle through exclusive membership."
+        />
+        <meta property="og:image" content="https://i.imgur.com/5sQQ2bY.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="https://www.biellavita.com/club" />
+      </Head>
+      <main className="homepage">
+        {/* <video autoPlay muted loop className="background-video">
         <source src="/Slowed background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video> */}
-      <div className="overlay"></div>
-      <MainHeading />
-      <Footer />
-    </main>
+        <div className="overlay"></div>
+        <MainHeading />
+        <Footer />
+      </main>
+    </>
   );
 }
