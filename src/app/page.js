713131f896a7615b16ec786839cbd926a7ec5306
aitizaz <atz.softprgmr@gmail.com>
2024-10-28 09:32:30 +0500

    added changes

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,23 @@ export const metadata = {
   title: "Home | BiellaVita",
   description: "Private social club",
   canonical: "https://www.biellavita.com",
+  icons: {
+    icon: "https://i.imgur.com/5sQQ2bY.png", // Reference your favicon here
+    apple: "https://i.imgur.com/5sQQ2bY.png", // Optional, for Apple devices
+  },
+  openGraph: {
+    url: "https://www.biellavita.com",
+    title: "Home | BiellaVita",
+    description: "Private social club",
+    images: [
+      {
+        url: "https://i.imgur.com/5sQQ2bY.png", // Link to an Imgur-hosted image for social sharing
+        width: 1200,
+        height: 630,
+        alt: "Biella Vita social preview",
+      },
+    ],
+  },
 };
 
 export default function Home() {
