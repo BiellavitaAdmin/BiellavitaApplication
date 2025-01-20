77bc8954ba111b3003f4abb54b15419f62524e3f
aitizaz <atz.softprgmr@gmail.com>
2025-01-20 16:59:16 +0500

    added changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -15,7 +15,17 @@ module.exports = {
     "/api/*", // Exclude all API routes
     "/changepassword", // Exclude restricted pages
     "/login", // Exclude login page
-  ], // Exclude restricted/private routes
+  ],
+  async additionalPaths() {
+    return [
+      { loc: "/", priority: 1.0, changefreq: "daily" }, // Homepage
+      { loc: "/club", priority: 0.8, changefreq: "daily" }, // Club page
+      { loc: "/visionandmission", priority: 0.7, changefreq: "daily" }, // Vision & Mission
+      { loc: "/projects", priority: 0.7, changefreq: "daily" }, // Projects page
+      { loc: "/partnership", priority: 0.7, changefreq: "daily" }, // Partnership page
+      { loc: "/getintouch", priority: 0.7, changefreq: "daily" }, // Get in Touch page
+    ];
+  },
   robotsTxtOptions: {
     policies: [
       {
